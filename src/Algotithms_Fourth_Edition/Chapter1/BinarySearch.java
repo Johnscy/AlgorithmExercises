78a8a416f50c35d77f1c1a14f35077f64dea41da
Johnscy <847841292@qq.com>
2018-05-11 22:27:27 +0800

    now

--- src/Algotithms_Fourth_Edition/Chapter1/BinarySearch.java
+++ src/Algotithms_Fourth_Edition/Chapter1/BinarySearch.java
@@ -1,4 +1,6 @@
+package Algotithms_Fourth_Edition.Chapter1;
 import java.util.Arrays;
+import edu.princeton.cs.algs4.*;
 public class BinarySearch{
 	public static int rank(int key,int a[]){
 		int lo = 0;
@@ -13,8 +15,8 @@ public class BinarySearch{
 	}
 
 	public static void main(String args[]){
-		int whitelist = In.readInts(args[0]);
-		Array.sort(whitelist);
+		int[] whitelist = In.readInts(args[0]);
+		Arrays.sort(whitelist);
 		while(!StdIn.isEmpty()){
 			int key = StdIn.readInt();
 			if(rank(key,whitelist) < 0)
---
+++ src/Algotithms_Fourth_Edition/Chapter1/ex1_3_9.java
@@ -0,0 +1,8 @@
+package Algotithms_Fourth_Edition.Chapter1;
+import edu.princeton.cs.algs4.StdIn;
+import java.util.*;
+public class ex1_3_9 {
+    public static void main(String args[]){
+
+    }
+}
