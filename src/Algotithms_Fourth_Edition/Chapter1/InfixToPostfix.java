72ee064990389ea11488496c8b0bdcf910969e24
Johnscy <847841292@qq.com>
2018-05-13 22:03:31 +0800

    = =

--- src/Algotithms_Fourth_Edition/Chapter1/InfixToPostfix.java
+++ src/Algotithms_Fourth_Edition/Chapter1/InfixToPostfix.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class InfixToPostfix {
     public static void main(String args[]) {
-        Stack<String> stack = new Stack<>();
+        Stack<String> stack = new Stack<String>();
         Scanner sc = new Scanner(System.in);
         String[] inputs = sc.nextLine().split("");
         for (String input : inputs) {
@@ -27,15 +27,15 @@ public class InfixToPostfix {
                 }
             }
             else if (input.equals("+") || input.equals("-") || input.equals("*") || input.equals("/")) {     //当输入是运算符op（+、- 、×、/）时候
-                boolean isLowerPriority = false;
+                boolean isStackTopLowerPriority = false;
                 boolean isLeftParen = false;
                 if (!stack.isEmpty()) {
                     String ops = stack.peek();
                     isLeftParen = stack.peek().equals("(");
-                    if ((input.equals("*") || input.equals("/")) && ops.equals("+") || ops.equals("-"))
-                        isLowerPriority = true;
+                    if ((input.equals("*") || input.equals("/")) && (ops.equals("+") || ops.equals("-")))
+                        isStackTopLowerPriority = true;
                 }
-                if (!(stack.isEmpty() || isLowerPriority || isLeftParen)) {             //a)循环，当（栈非空and栈顶不是开括号and栈顶运算符的优先级不低于输入的运算符的优先级）时，反复操作：将栈顶元素出栈输出
+                if (!stack.isEmpty() && !isStackTopLowerPriority && !isLeftParen) {             //a)循环，当（栈非空and栈顶不是开括号and栈顶运算符的优先级不低于输入的运算符的优先级）时，反复操作：将栈顶元素出栈输出
                     while (!stack.isEmpty())
                         System.out.print(stack.pop());
                 }
@@ -49,7 +49,8 @@ public class InfixToPostfix {
                 System.out.print("error");
                 break;
             } else
-                System.out.println(s);
+                System.out.print(s);
         }
+        System.out.println();
     }
 }
