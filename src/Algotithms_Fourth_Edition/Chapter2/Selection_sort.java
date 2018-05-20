b823004252f939a79f42e02c6b04ad76d62d281f
Johnscy <847841292@qq.com>
2018-05-20 14:16:46 +0800

    = =

---
+++ src/Algotithms_Fourth_Edition/Chapter2/Selection_sort.java
@@ -0,0 +1,60 @@
+package Algotithms_Fourth_Edition.Chapter2;
+
+import java.util.Scanner;
+
+public class Selection_sort {
+    public static void sort(Comparable[] a){
+        int N = a.length;
+        for(int i = 0;i < N;i++){
+            int min = i;
+            for (int j = i + 1;j < N;j++){
+                if(less(a[j],a[min]))  min = j;
+            }
+            exch(a,i,min);
+        }
+    }
+    private static boolean less(Comparable v,Comparable w){
+        return v.compareTo(w) < 0;
+    }
+    private static void exch(Comparable[] a,int i,int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+    private static boolean isSorted(Comparable[] a){
+        for (int i = 1;i < a.length;i++){
+            if(less(a[i],a[i-1])) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        String[] input = null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一组数字/字母，并用空格隔开：");
+//      while(sc.hasNext())
+        input = sc.nextLine().split("\\s");
+        if(input[0].matches("\\d+?([.]\\d+)")) {         //判断输入是整数或者小数（当然均为正的了XD），然后转为Double型比较
+            Double[] inputNums = new Double[input.length];
+            for(int k = 0;k < input.length;k++) {
+                inputNums[k] = Double.parseDouble(input[k]);
+            }
+            sort(inputNums);
+            System.out.print(isSorted(inputNums));
+            System.out.println();
+            show(inputNums);
+        } else{                                                 //否则，输入的字母。。。直接比字符串大小吧
+            sort(input);
+            System.out.print(isSorted(input));
+            System.out.println();
+            show(input);
+        }
+
+    }
+
+}
