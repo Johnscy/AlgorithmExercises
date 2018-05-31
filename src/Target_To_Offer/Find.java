ad3dc43657c754482257b15ce96b7a7bd7f7e08a
Johnscy <847841292@qq.com>
2018-05-31 20:47:49 +0800

    = =

---
+++ src/Target_To_Offer/Find.java
@@ -0,0 +1,24 @@
+package Target_To_Offer;
+
+/*
+    在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ */
+public class Solution {
+    public boolean Find(int target, int [][] array) {
+        boolean found = false;
+        if (array != null) {
+            int i = 0;
+            int j = array[i].length - 1;
+            while (i < array.length && j >= 0) {
+                if (array[i][j] == target) {
+                    found = true;
+                    break;
+                } else if (array[i][j] > target)
+                    j--;
+                else
+                    i++;
+            }
+        }
+        return found;
+    }
+}
