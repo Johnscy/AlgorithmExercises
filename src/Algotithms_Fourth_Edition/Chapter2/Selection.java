f431300404ff0b8fa9b9951fcf92a90cb62a8d6b
Johnscy <847841292@qq.com>
2018-06-03 20:59:51 +0800

    选择数组中第k小的元素

---
+++ src/Algotithms_Fourth_Edition/Chapter2/Selection.java
@@ -0,0 +1,66 @@
+package Algotithms_Fourth_Edition.Chapter2;
+import java.util.*;
+
+/**
+ * 找到一组数中第k小的元素
+ */
+public class Selection {
+    private static int partition(Comparable[] a, int lo, int hi){
+        int i = lo,j = hi+1;
+        Comparable v = a[lo];
+        while(true){
+            while(less(a[++i],v)) if(i == hi) break;
+            while(less(v,a[--j])) if(j == lo) break;
+            if(i >= j) break;
+            exch(a,i,j);
+        }
+        exch(a,lo,j);
+        return j;
+    }
+
+    private static Comparable select(Comparable[] a, int k){
+        List<Comparable> list = new LinkedList<Comparable>();
+        for (int x = 0;x < a.length ;x++){
+            list.add(a[x]);
+        }
+        Collections.shuffle(list);             //利用List的shuffle方法打乱数组XD
+        for (int x = 0;x < a.length ;x++){
+            a[x] = list.get(x);
+        }
+        int lo = 0,hi = a.length - 1;
+        while(hi > lo){
+            int j = partition(a,lo,hi);
+            if(j == k)  return a[k-1];
+            else if(j > k) hi = j - 1;
+            else if(j < k) lo = j + 1;
+        }
+        return a[k-1];//数组从0开始计数，第k小要减1
+    }
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+    public static void main(String[] args){
+        String[] input;
+        int k;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一组数字/字母，并用空格隔开：");
+        input = sc.nextLine().split("\\s");
+        System.out.println("请输入一个数字，表示选择输入数组中第k小的元素");
+        k = sc.nextInt();
+        if (input[0].matches("\\d+([.]\\d+)?")) {         //判断输入是整数或者小数（当然均为正的了XD），然后转为Double型比较
+            Double[] inputNums = new Double[input.length];
+            for (int x = 0; x < input.length; x++) {
+                inputNums[x] = Double.parseDouble(input[x]);
+            }
+            System.out.println(select(inputNums,k));
+        } else {                                                 //否则，输入的字母。。。直接比字符串大小吧
+            System.out.println(select(input,k));
+        }
+    }
+}
