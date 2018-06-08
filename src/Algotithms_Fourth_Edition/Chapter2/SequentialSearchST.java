8cc1bdf8188463ab0457a3c11de9b8d529fd6c78
Johnscy <847841292@qq.com>
2018-06-08 19:54:56 +0800

    顺序查找（基于无序链表）

--- src/Algotithms_Fourth_Edition/Chapter2/SequentialSearchST.java
+++ src/Algotithms_Fourth_Edition/Chapter2/SequentialSearchST.java
@@ -1,5 +1,5 @@
 package Algotithms_Fourth_Edition.Chapter2;
-
+import java.util.*;
 /**
  * 顺序查找，基于无序链表
  */
@@ -20,6 +20,7 @@ public class SequentialSearchST<Key,Value> {
     private int N = 0;          //链表结点数
 
     public Value get(Key key){      //查找给定的键，返回相关联的值
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
         for (Node x = first;x != null;x = x.next){
             if (key.equals(x.key))
                 return x.val;       //命中
@@ -47,12 +48,12 @@ public class SequentialSearchST<Key,Value> {
     private Node delete(Node x,Key key){
         if (x == null) return null;
         if (key.equals(x.key)){N--;return x.next;}
-        x.next = delete(x.next,key);
+        x.next = delete(x.next,key);    //递归查询每个结点
+        return x;       //返回头结点
     }
     public boolean contains(Key key){
-        for (Node x = first;x != null;x = x.next)
-            if (key.equals(x.key)) return true;
-        return false;
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
     }
     public boolean isEmpty(){
         return size() == 0;
@@ -61,10 +62,20 @@ public class SequentialSearchST<Key,Value> {
         return N;
     }
     public Iterable<Key> keys(){
-
+        Queue<Key> queue = new LinkedList<Key>();
+        for (Node x = first; x != null; x = x.next)
+            queue.offer(x.key);
+        return queue;
     }
 
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        SequentialSearchST<String, Integer> st = new SequentialSearchST<String, Integer>();
+        for (int i = 0;sc.hasNext();i++) {
+            String key = sc.next();     //ctrl+D结束输入
+            st.put(key, i);
+        }
+        for (String s : st.keys())
+            System.out.println(s + " " + st.get(s));
     }
 }
