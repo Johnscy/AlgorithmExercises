aa64479a189d497b5d51cc124b69ae535c7f02f7
Johnscy <847841292@qq.com>
2018-06-13 19:55:19 +0800

    归并排序（自底向上）

--- src/Algotithms_Fourth_Edition/Chapter2/MergeBU_sort.java
+++ src/Algotithms_Fourth_Edition/Chapter2/MergeBU_sort.java
@@ -46,7 +46,6 @@ public class MergeBU_sort {                  //归并的自底向上实现
         String[] input;
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入一组数字/字母，并用空格隔开：");
-//      while(sc.hasNext())
         input = sc.nextLine().split("\\s");
         if(input[0].matches("\\d+([.]\\d+)?")) {         //判断输入是整数或者小数（当然均为正的了XD），然后转为Double型比较
             Double[] inputNums = new Double[input.length];
