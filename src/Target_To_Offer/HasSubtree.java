5543e3b2f7e60a9a4c6dcd3dda00828e9bfdcb15
Johnscy <847841292@qq.com>
2018-06-28 00:53:41 +0800

    树的子结构

---
+++ src/Target_To_Offer/HasSubtree.java
@@ -0,0 +1,22 @@
+package Target_To_Offer;
+
+/**
+ * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+ */
+public class HasSubtree {
+     public class TreeNode {
+         int val = 0;
+         TreeNode left = null;
+         TreeNode right = null;
+
+         public TreeNode(int val) {
+         this.val = val;
+         }
+     }
+
+    public class Solution {
+        public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+
+        }
+    }
+}
