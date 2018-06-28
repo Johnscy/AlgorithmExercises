4cf62b4c23046c14ec17b02cf5bf2604ee1dd289
Johnscy <847841292@qq.com>
2018-06-29 01:31:34 +0800

    顺时针打印矩阵

---
+++ src/Target_To_Offer/PrintMatrix.java
@@ -0,0 +1,33 @@
+package Target_To_Offer;
+import java.util.ArrayList;
+
+/**
+ * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+ * 例如，如果输入如下矩阵： 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+ * 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+ */
+public class PrintMatrix {
+    public class Solution {
+        public ArrayList<Integer> printMatrix(int [][] matrix) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+//            int row = matrix.length;      //行数
+//            int column = matrix[0].length;   //列数
+            int r1 = 0, r2 = matrix.length - 1;
+            int c1 = 0, c2 = matrix[0].length - 1;
+            while (r1 <= r2 && c1 <= c2){
+                for (int i = c1;i <= c2;i++)
+                    arrayList.add(matrix[r1][i]);
+                for (int i = r1 + 1;i <= r2;i++)
+                    arrayList.add(matrix[i][c2]);
+                if (r1 != r2)
+                    for (int i = c2 - 1;i >= c1;i--)
+                        arrayList.add(matrix[r2][i]);
+                if (c1 != c2)
+                    for (int i = r2 - 1;i > r1;i--)
+                        arrayList.add(matrix[i][c1]);
+                r1++;r2--;c1++;c2--;
+            }
+            return arrayList;
+        }
+    }
+}
