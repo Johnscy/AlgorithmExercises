6ee94d7a832143137318f766b258527a730a13ee
Johnscy <847841292@qq.com>
2018-07-01 21:56:58 +0800

    二叉搜索树与双向链表

--- src/Target_To_Offer/Convert.java
+++ src/Target_To_Offer/Convert.java
@@ -1,11 +1,11 @@
 package Target_To_Offer;
-
+import java.util.Stack;
 /**
  * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。
  * 要求不能创建任何新的结点，只能调整树中结点指针的指向。
  */
 public class Convert {
-     public class TreeNode {
+    public class TreeNode {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
@@ -13,13 +13,56 @@ public class Convert {
         public TreeNode(int val) {
             this.val = val;
         }
-     }
+    }
 
-    public class Solution {
+    //递归
+    public class Solution_recursion {
+        private TreeNode pre = null;    //暂存上个结点
+        private TreeNode head = null;
         public TreeNode Convert(TreeNode pRootOfTree) {
             if (pRootOfTree == null) return null;
             inOrder(pRootOfTree);
-            return
+            return head;
+        }
+
+        private void inOrder(TreeNode node){
+            if (node == null)
+                return;
+            inOrder(node.left);
+            if (head == null)
+                head = node;    //用head指向头结点，即左子树的最左的叶子结点
+            node.left = pre;    //node结点的左指针指向它的左结点
+            if (pre != null)
+                pre.right = node;//node左结点的右指针指向node
+            pre = node;          //继续推进
+            inOrder(node.right);
+        }
+    }
+
+    //非递归
+    //用栈来进行中序遍历
+    public class Solution {
+        public TreeNode Convert(TreeNode pRootOfTree) {
+            if (pRootOfTree == null)
+                return null;
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode node = null;
+            TreeNode head = null;
+            while (pRootOfTree != null || !stack.isEmpty()){
+                while (pRootOfTree != null){
+                    stack.push(pRootOfTree);
+                    pRootOfTree = pRootOfTree.left;
+                }
+                pRootOfTree = stack.pop();
+                if (head == null)
+                    head = pRootOfTree;
+                if (node != null)
+                    node.right = pRootOfTree;
+                pRootOfTree.left = node;
+                node = pRootOfTree;
+                pRootOfTree = pRootOfTree.right;
+            }
+            return head;
         }
     }
 }
