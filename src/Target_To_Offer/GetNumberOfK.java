078c001dbaa43d7fe1a9879612f9230c3c26dd3d
Johnscy <847841292@qq.com>
2018-07-07 22:44:50 +0800

    数字在排序数组中出现的次数

---
+++ src/Target_To_Offer/GetNumberOfK.java
@@ -0,0 +1,36 @@
+package Target_To_Offer;
+
+/**
+ * 统计一个数字在排序数组中出现的次数。
+ */
+public class GetNumberOfK {
+    //二分查找QAQ
+    public class Solution {
+        public int GetNumberOfK(int [] array , int k) {
+            if (array == null || array.length == 0)
+                return 0;
+            int FirstK = BinarySearch(array,0,array.length,k);
+            int LastK = BinarySearch(array,0,array.length,k + 1);
+            return (FirstK == array.length || array[FirstK] != k) ? 0 : (LastK - FirstK);
+        }
+        //递归写法
+        private int BinarySearch(int[] arr, int lo, int hi, int k){
+            if (lo >= hi)   return -1;
+            int mid = (lo + hi) >> 1;
+            if (arr[mid] >= k)           return BinarySearch(arr,lo,mid - 1,k);
+            else if (arr[mid] < k)      return BinarySearch(arr,mid + 1,hi,k);
+            return lo;
+        }
+        //非递归，循环写法
+/*        private int BinarySearch(int[] arr, int lo, int hi, int k){
+            while (lo < hi) {
+                int mid = (lo + hi) >> 1;
+                if (arr[mid] >= k) //!!!!!!!
+                    hi = mid;
+                else if (arr[mid] < k)
+                    lo = mid + 1;
+            }
+            return lo;
+        }*/
+    }
+}
