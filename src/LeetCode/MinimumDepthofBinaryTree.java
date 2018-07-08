bd2e0b0508c6f836327a3b0eae503afb8902b4eb
Johnscy <847841292@qq.com>
2018-07-09 00:26:24 +0800

    二叉树的最小深度

--- src/LeetCode/MinimumDepthofBinaryTree.java
+++ src/LeetCode/MinimumDepthofBinaryTree.java
@@ -38,7 +38,11 @@ public class MinimumDepthofBinaryTree {
         }
     }
 
-    //递归
+    //递归：
+    //若为空树返回0；
+    //若左子树为空，则返回右子树的最小深度+1；（加1是因为要加上根这一层，下同）
+    //若右子树为空，则返回左子树的最小深度+1；
+    //若左右子树均不为空，则取左、右子树最小深度的较小值，+1；
     public class Solution_Recursion {
         public int run(TreeNode root) {
             if(root == null)
