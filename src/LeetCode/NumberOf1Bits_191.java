45ccadca1087d9b415b14ccc9df784fe409e8c2a
Johnscy <847841292@qq.com>
2018-07-19 20:47:11 +0800

    数字中1的个数（位操作）

--- src/LeetCode/NumberOf1Bits_191.java
+++ src/LeetCode/NumberOf1Bits_191.java
@@ -3,7 +3,7 @@ package LeetCode;
 /**
  * Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
  */
-public class NumberOf1Bits {
+public class NumberOf1Bits_191 {
     //位操作
     public class Solution {
         // you need to treat n as an unsigned value
