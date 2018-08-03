2ab8944fe136658cb572ed1cc1a60548255b04a6
Johnscy <847841292@qq.com>
2018-08-03 20:44:29 +0800

    双元素的链的最长长度（DP/without DP）

--- src/LeetCode/MaximumLengthOfPairChain_646.java
+++ src/LeetCode/MaximumLengthOfPairChain_646.java
@@ -18,12 +18,19 @@ public class MaximumLengthOfPairChain_646 {
             if (pairs == null || pairs.length == 0)
                 return 0;
             Arrays.sort(pairs,(o1, o2) -> (o1[0] - o2[0])); //将数组按第一个元素递增的顺序排序
-            int[] dp = new int[pairs.length];
-
+            int[] dp = new int[pairs.length];   //到第i个pair为止，最长的链的长度
+            Arrays.fill(dp,1);
+            for (int i = 0; i < dp.length; i++)
+                for (int j = i - 1; j >= 0; j--)
+                    if (pairs[i][0] > pairs[j][1]){
+                        dp[i] = dp[j] + 1;
+                        break;
+                    }
+            return dp[pairs.length - 1];
         }
     }
 
-    //
+    //without DP
     class Solution {
         public int findLongestChain(int[][] pairs) {
             if (pairs == null || pairs.length == 0)
@@ -34,8 +41,8 @@ public class MaximumLengthOfPairChain_646 {
                 if (pair[0] > pre) {
                     len++;
                     pre = pair[1];
-                }else if (pre < pair[1])
-                    pre = pair[1];
+                }else if (pre > pair[1])    //在当前连接断了的情况下（当前的pair中的a小于前一个pair的b），判断前一个pair[a,b]中b是否比当前pair的b大
+                    pre = pair[1];          //是的话，抛弃之前的pair，将当前pair纳入链中
             }
             return len;
         }
