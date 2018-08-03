6ab3d2039d70136da33cfece1f7e619f38aa285d
Johnscy <847841292@qq.com>
2018-08-04 01:57:52 +0800

    买进卖出商品（需要交交易费）的最好时机（DP）

--- src/LeetCode/BestTimeToBuyAndSellStockWithTransactionFee_714.java
+++ src/LeetCode/BestTimeToBuyAndSellStockWithTransactionFee_714.java
@@ -1,7 +1,5 @@
 package LeetCode;
 
-import edu.princeton.cs.algs4.In;
-
 /**
  * Your are given an array of integers prices, for which the i-th element is the price of a given stock on day i;
  * and a non-negative integer fee representing a transaction fee.
@@ -26,16 +24,13 @@ public class BestTimeToBuyAndSellStockWithTransactionFee_714 {
         public int maxProfit(int[] prices, int fee) {
             if (prices == null || prices.length == 0 || fee < 0)
                 return 0;
-            int profit = 0, temp = 0, buy = Integer.MAX_VALUE, sell;
-            //int[] dp = new int[prices.length];
+            int T_ik0 = 0,  T_ik1 = Integer.MIN_VALUE;  //T_ik0 : T[i][k][0]，第i天，整个过程允许最多交易k次（此处为prices.length / 2），手上拥有0件商品
             for (int price : prices){
-                if (price < buy)
-                    buy = price;
-                if (price - buy - fee > 0)
-                    temp = price - buy - fee;
-
-                    profit += ;
+                int T_ik0_old = T_ik0;
+                T_ik0 = Math.max(T_ik0_old,T_ik1 + price);
+                T_ik1 = Math.max(T_ik1,T_ik0_old - price - fee);
             }
+            return T_ik0;
         }
     }
 }
