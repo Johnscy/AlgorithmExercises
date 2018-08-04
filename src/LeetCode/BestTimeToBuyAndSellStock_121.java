a8e6274af920927ec043b6f1bfa12e7294cc945e
Johnscy <847841292@qq.com>
2018-08-04 13:27:57 +0800

    买入和卖出库存的最佳时间（DP）

--- src/LeetCode/BestTimeToBuyAndSellStock_121.java
+++ src/LeetCode/BestTimeToBuyAndSellStock_121.java
@@ -17,6 +17,10 @@ package LeetCode;
  * Explanation: In this case, no transaction is done, i.e. max profit = 0.
  */
 public class BestTimeToBuyAndSellStock_121 {
+    //DP
+    //通用公式：i为第i个交易日（0到prices.length - 1）；k为最多交易次数；第三项为当前持有的商品个数0或1（规定：必须在再一次购买之前卖掉商品）
+    //T[i][k][0] = max(T[i - 1][k][0], T[i - 1][k][1] + prices[i])
+    //T[i][k][1] = max(T[i - 1][k][1], T[i - 1][k - 1][0] - prices[i])
     class Solution {
         public int maxProfit(int[] prices) {
             if(prices == null || prices.length == 0)
