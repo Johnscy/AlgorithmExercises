9eb3d6c2395a5c811b0eb0996900a0519ca0dc0a
Johnscy <847841292@qq.com>
2018-08-11 12:06:33 +0800

    最长公共子串：The Longest Common Substring

---
+++ src/Exercises/LCS.java
@@ -0,0 +1,38 @@
+package Exercises;
+import java.util.*;
+
+//最长公共子串：The Longest Common Substring
+//与最长公共子序列(The Longest Common Subsequence)不同
+public class LCS{
+    public static void main(String args[]){
+        Scanner sc  =   new Scanner(System.in);
+        char[] str1 =   sc.next("\\w+").toCharArray();
+        char[] str2 =   sc.next("\\w+").toCharArray();
+        int dp[][]  =   new int[str1.length][str2.length];
+        int maxLen  =   0;
+        int end     =   0;
+        for(int i = 0;i < str1.length;i++) {
+            for (int j = 0; j < str2.length; j++) {
+                if (str1[i] == str2[j])
+                    if (i == 0 || j == 0)
+                        dp[i][j] = 1;
+                    else
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = 0;
+                if(dp[i][j] > maxLen){
+                    maxLen = dp[i][j];
+                    end = i;
+                }
+            }
+        }
+        System.out.print("result = ");
+        if(maxLen == 0)
+            System.out.print("null");
+        else
+            for(int k = end - maxLen + 1;k <= end;k++){
+                System.out.print(str1[k]);
+            }
+        System.out.println();
+    }
+}
