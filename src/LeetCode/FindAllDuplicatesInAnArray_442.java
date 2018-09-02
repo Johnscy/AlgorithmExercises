5a7b80fefb3b141cf225794a6c6d335d199e4ca7
Johnscy <847841292@qq.com>
2018-09-02 14:25:38 +0800

    找到数组中的所有重复元素（Math/Array）

--- src/LeetCode/FindAllDuplicatesInAnArray_442.java
+++ src/LeetCode/FindAllDuplicatesInAnArray_442.java
@@ -14,7 +14,8 @@ import java.util.*;
  */
 public class FindAllDuplicatesInAnArray_442 {
     // nums[i]∈[1,nums.length]
-    class Solution {
+    //with swapping.O(n) runtime
+    class Solution_Swapping {
         public List<Integer> findDuplicates(int[] nums) {
             List<Integer> res = new ArrayList<>();
             if (nums == null || nums.length == 0)
@@ -41,12 +42,33 @@ public class FindAllDuplicatesInAnArray_442 {
         }
     }
 
+    //// nums[i]∈[1,nums.length]
+    //when find a number i, flip the number at position i-1 to negative.
+    //if the number at position i-1 is already negative, i is the number that occurs twice.
+    //O(n) runtime
+    class Solution_SetNegative {
+        public List<Integer> findDuplicates(int[] nums) {
+            List<Integer> res = new ArrayList<>();
+            if (nums == null || nums.length == 0)
+                return res;
+            int index;
+            for (int i = 0; i < nums.length; i++) {
+                index = Math.abs(nums[i]) - 1;
+                if (nums[index] < 0)
+                    res.add(Math.abs(index + 1));
+                nums[index] = - nums[index];
+            }
+            return res;
+        }
+    }
+
+    //test
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] str = sc.nextLine().split(",");
         int[] nums = new int[str.length];
         for (int i = 0; i < nums.length; i++)
             nums[i] = Integer.valueOf(str[i]);
-        System.out.print(new FindAllDuplicatesInAnArray_442().new Solution().findDuplicates(nums));
+        System.out.print(new FindAllDuplicatesInAnArray_442().new Solution_SetNegative().findDuplicates(nums));
     }
 }
