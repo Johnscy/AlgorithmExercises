1830cce4f8d3012e9987e5181ec900f6de42be0c
Johnscy <847841292@qq.com>
2018-09-02 14:45:23 +0800

    两个数组中重复的子数组的最大长度（DP）

--- src/LeetCode/MaximumLengthOfRepeatedSubarray_718.java
+++ src/LeetCode/MaximumLengthOfRepeatedSubarray_718.java
@@ -22,12 +22,11 @@ public class MaximumLengthOfRepeatedSubarray_718 {
             int[][] dp = new int[m + 1][n + 1]; //dp[i][j] : 从1 - i和从1 - j的重复子数组长度
             int max = 0;
             for (int i = 1; i <= m; i++)
-                for (int j = 1; j <= n; j++) {
+                for (int j = 1; j <= n; j++)
                     if (A[i - 1] == B[j - 1]) {
                         dp[i][j] = dp[i - 1][j - 1] + 1;
                         max = Math.max(dp[i][j], max);
                     }
-                }
             return max;
         }
     }
