6eac444b847486c97a3eeffee200e606a155e761
Johnscy <847841292@qq.com>
2018-09-03 01:12:12 +0800

    能够构成三角形的数（3-pointers）

--- src/LeetCode/ValidTriangleNumber_611.java
+++ src/LeetCode/ValidTriangleNumber_611.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Given an array consists of non-negative integers,
@@ -15,18 +16,33 @@ import java.util.Arrays;
  * 2,2,3
  */
 public class ValidTriangleNumber_611 {
+    //3 - sum的解法
     class Solution {
         public int triangleNumber(int[] nums) {
             if (nums == null || nums.length < 3)
                 return 0;
-            int res = 0;
+            int count = 0;
             Arrays.sort(nums);
-            for (int i = 0; i <= nums.length - 3; i++) {
-                int count = 0;
-                for (int j = i + 1; j <= nums.length - 2; j++) {
-                    if (nums[i] + nums[j])
+            for (int i = 2; i < nums.length; i++) {
+                int l = 0, r = i - 1;
+                while (l < r) {
+                    if (nums[l] + nums[r] > nums[i]) {
+                        count += r - l;
+                        r--;
+                    } else
+                        l++;
                 }
             }
+            return count;
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(",");
+        int[] nums = new int[str.length];
+        for (int i = 0; i < nums.length; i++)
+            nums[i] = Integer.valueOf(str[i]);
+        System.out.print(new ValidTriangleNumber_611().new Solution().triangleNumber(nums));
     }
 }
