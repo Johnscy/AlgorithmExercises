9eea26a82d91aac3b4348a79c70ef7eb11283eae
Johnscy <847841292@qq.com>
2018-09-06 22:28:11 +0800

    = =

--- src/Exercises/Test.java
+++ src/Exercises/Test.java
@@ -775,6 +775,50 @@ class Main {
 
     }*/
 
+class Main {
+    //5 3 2
+    //3 1 1 1 2
+    //out: 3
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int t = sc.nextInt();
+        sc.nextLine();
+        int[] a = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+            a[i] = sc.nextInt();
+        int[] numAppearTimes = new int[100000];
+        int res = 0;
+        if (k >= n){
+            for (int i = 1; i <= n; i++)
+                numAppearTimes[a[i]]++;
+            for (int i = 1; i <= n; i++) {
+                if(numAppearTimes[a[i]] >= t) {
+                    System.out.println(1);
+                    return;
+                }
+            }
+            System.out.println(0);
+        }else {
+            //int[] count = new int[n - k + 1 + 1];
+            for (int j = 1; j <= k; j++)
+                numAppearTimes[a[j]]++;
+            for (int i = 1; i <= n - k; i++) {
+                for (int j = i; j <= i + k - 1; j++) {
+                    if (a[j] == a[j + 1] && j < i + k -1)
+                        continue;
+                    if (numAppearTimes[a[j]] >= t)
+                        res++;//count[i]++;
+                }
+                numAppearTimes[a[i + k]]++;
+                numAppearTimes[a[i]]--;
+            }
+            System.out.println(res);
+        }
+    }
+}
+
 
 
 
