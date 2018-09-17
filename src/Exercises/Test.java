47cd5554ada0ff43bab530a7325336926374ae84
Johnscy <847841292@qq.com>
2018-09-17 16:32:53 +0800

    = =

--- src/Exercises/Test.java
+++ src/Exercises/Test.java
@@ -1092,6 +1092,129 @@ class Main {
     }
 }*/
 
+//class Main{
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int T = sc.nextInt();sc.nextLine();
+//        for (int i = 0; i < T; i++) {
+//            int N = sc.nextInt();
+//            int M = sc.nextInt();
+//            if (M == 0 || N == 1) {
+//                System.out.println("Yes");
+//                continue;
+//            }
+//            int[][] matrix = new int[N + 1][N + 1];
+//            for (int j = 0; j < M; j++) {
+//                int x = sc.nextInt();int y = sc.nextInt();sc.nextLine();
+//                matrix[x][y] = 1;
+//                matrix[y][x] = 1;
+//            }
+//            boolean res = yesOrNo(matrix);
+//            System.out.println(res ? "Yes" : "No");
+//        }
+//    }
+//
+//    private static boolean yesOrNo(int[][] matrix){
+//        for (int i = 1; i < matrix.length; i++) {
+//            for (int j = 1; j < matrix.length; j++) {
+//                if (matrix[i][j] == 0)
+//                    continue;
+//
+//            }
+//        }
+//    }
+//}
+
+
+//class Main{
+//    public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        int T = sc.nextInt();
+//        while(T-->0){
+//            int N = sc.nextInt();
+//            int M = sc.nextInt();
+//            ArrayList<HashSet<Integer>> map = new ArrayList<>();
+//            for(int i = 0;i < N;i++)
+//                map.add(new HashSet<>());
+//            for(int i = 0;i < M;i++){
+//                int x = sc.nextInt();
+//                int y = sc.nextInt();
+//                map.get(x - 1).add(y - 1);
+//                map.get(y - 1).add(x - 1);
+//            }
+//            ArrayList<ArrayList<Integer>> tmp = new ArrayList<>();
+//            int flag=0;
+//            for(int i = 0;i < N;i++){
+//                for(int j = 0;j < tmp.size();j++){
+//                    ArrayList<Integer> list = tmp.get(j);
+//                    flag=0;
+//                    int stateNode = list.get(0);
+//                    if(map.get(i).contains(stateNode)){
+//                        flag=1;
+//                    }else flag = -1;
+//                    for(int k = 0;k < list.size();k++){
+//                        if((flag == 1 && !map.get(i).contains(list.get(k)))||(flag == -1 && map.get(i).contains(list.get(k)))){
+//                            flag = -2;
+//                            break;
+//                        }
+//                    }
+//                    if(flag == -2)break;
+//                    if(flag == -1){
+//                        if(helper(map,tmp,i,j)){
+//                            list.add(i);
+//                        }
+//                        else
+//                            flag = -2;
+//                        break;
+//                    }
+//                }
+//                if(flag == 0 || flag == 1){
+//                    ArrayList<Integer> newList=new ArrayList<>();
+//                    newList.add(i);
+//                    tmp.add(newList);
+//                }
+//            }
+//            if(flag == -2)System.out.println("No");
+//            else System.out.println("Yes");
+//        }
+//    }
+//    static boolean helper(ArrayList<HashSet<Integer>> map, ArrayList<ArrayList<Integer>> tmp,int m,int n){
+//        for(int i = 0;i < tmp.size();i++){
+//            if(i == n)continue;
+//            ArrayList<Integer> list = tmp.get(i);
+//            for(int j = 0;j < list.size();j++){
+//                if(!map.get(m).contains(list.get(j)))
+//                    return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
+
+// class Main{
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();sc.nextLine();
+//        int[][] input = new int[n][3];
+//        for (int i = 0; i < n; i++) {
+//            input[i][0] = sc.nextInt();
+//            input[i][1] = sc.nextInt();
+//            input[i][2] = sc.nextInt();
+//            sc.nextLine();
+//        }
+//        int res = 0;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (input[j][0] > input[j][0] && input[j][1] > input[i][1] && input[j][2] > input[i][2]){
+//                    res++;
+//                    break;
+//                }
+//            }
+//        }
+//        System.out.println(res);
+//    }
+//}
+
 
 
 
