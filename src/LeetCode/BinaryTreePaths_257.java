a71d163a7f1f0014cfca3b76e460619a9d7acb74
Johnscy <847841292@qq.com>
2018-09-25 13:43:58 +0800

    = =

---
+++ src/LeetCode/BinaryTreePaths_257.java
@@ -0,0 +1,40 @@
+package LeetCode;
+import java.util.*;
+
+/**
+ * Given a binary tree, return all root-to-leaf paths.
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ * Input:
+ *    1
+ *  /   \
+ * 2     3
+ *  \
+ *   5
+ * Output: ["1->2->5", "1->3"]
+ * Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+ */
+public class BinaryTreePaths_257 {
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    class Solution {
+        public List<String> binaryTreePaths(TreeNode root) {
+            List<String> paths = new LinkedList<>();
+            Stack<TreeNode> treeNodes = new LinkedList<>();
+            if (root == null)
+                return paths;
+            treeNodes.add(root);
+            while (!treeNodes.isEmpty()){
+
+            }
+        }
+    }
+}
