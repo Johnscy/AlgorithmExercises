764ebda0ec04fd6de3629c0af931ea9c76801d60
Johnscy <847841292@qq.com>
2018-10-10 22:30:00 +0800

    回溯法全排列

--- src/Exercises/Permutations.java
+++ src/Exercises/Permutations.java
@@ -5,17 +5,17 @@ package Exercises;
  */
 public class Permutations {
         public static void main(String[] args){
-            String s="abc";
+            String s = "abc";
             pailie(s,"");
         }
         public static void pailie(String s, String temp){//参数设计地尽量地简洁
-            if(s.length()==0){
+            if(s.length() == 0){
                 System.out.println(temp);
                 return;
             }
-            for(int i=0;i<s.length();i++){
-                String news=s.substring(0, i)+s.substring(i+1,s.length());//去掉String中的某个字母
-                pailie(news, temp+s.charAt(i));
+            for(int i=0;i < s.length();i++){
+                String news = s.substring(0, i) + s.substring(i + 1,s.length());//去掉String中的某个字母
+                pailie(news, temp + s.charAt(i));
             }
         }
 }
