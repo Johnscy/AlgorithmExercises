6737445cd6cfed652f93e79e1aa7cf8f348c4f18
Johnscy <847841292@qq.com>
2018-10-22 21:34:14 +0800

    = =

---
+++ src/LeetCode/RemoveLinkedListElements_203.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+/**
+ * Remove all elements from a linked list of integers that have value val.
+ *
+ * Example:
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ */
+public class RemoveLinkedListElements_203 {
+
+    //Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    //Recursion
+    class Solution_Recursion {
+        public ListNode removeElements(ListNode head, int val) {
+            if (head == null)
+        }
+    }
+}
