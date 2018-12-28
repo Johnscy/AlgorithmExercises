44015fe2144013f0dbf3eccebd90301cd5b14d5a
Johnscy <847841292@qq.com>
2018-12-28 14:38:27 +0800

    链表环的入口（fast pointer & slow pointer）

--- src/LeetCode/LinkedListCycleII_142.java
+++ src/LeetCode/LinkedListCycleII_142.java
@@ -30,7 +30,7 @@ public class LinkedListCycleII_142 {
         public ListNode detectCycle(ListNode head) {
             if (head == null || head.next == null)
                 return null;
-            ListNode fast = head,slow = head;
+            ListNode fast = head, slow = head;
             while (fast != null && fast.next != null){
                 fast = fast.next.next;
                 slow = slow.next;
