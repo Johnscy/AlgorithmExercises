da98959831a0163c68012661b97ac46edcb7ea7f
Johnscy <847841292@qq.com>
2019-05-28 17:38:20 +0800

    二叉平衡树给定范围内的和（Recursion）

--- src/LeetCode/RangeSumOfBST_938.java
+++ src/LeetCode/RangeSumOfBST_938.java
@@ -50,4 +50,14 @@ public class RangeSumOfBST_938 {
             }
         }
     }
+
+    //Recursion
+    class Solution_Best {
+        public int rangeSumBST(TreeNode root, int L, int R) {
+            if (root == null) return 0;
+            if (root.val > R) return rangeSumBST(root.left, L, R);
+            if (root.val < L) return rangeSumBST(root.right, L, R);
+            return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+        }
+    }
 }
